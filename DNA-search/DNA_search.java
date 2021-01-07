a84548c8aa969a5e7bf899317807ccaac1e0d46e
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-01-07 12:38:49 -0600

    first part

---
+++ DNA-search/DNA_search.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class DNASearch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        String sequence, subsequence;
+        boolean valid; 
+        char c;
