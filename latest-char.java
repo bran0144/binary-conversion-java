bdc33d33bfa2ecf2d4578f45917185933d932f7d
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-01-07 10:46:23 -0600

    looking at for loops

---
+++ latest-char.java
@@ -0,0 +1,11 @@
+String s = "The quick brown fox jumps over the lazy dog.";
+String lower = s.toLowerCase();
+char latest = ' ';
+char c;
+for(int i = 0; i < lower.length(); i++) {
+    c = lower.charAt(i);
+    if(c >= 'a' && c <= 'z' && c > latest)
+        latest = c;
+}
+System.out.println("The latest character in the alphabet from your message is: '"
+	+ latest + "'.");
